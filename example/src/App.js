4205c1e6425b00de50f350f67007d0fdceae63b1
TheGrandLynx <terence.ngama@gmail.com>
2024-02-21 09:57:38 +0100

    correct plugin

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,40 @@
 import React from 'react'
 
-import { ExampleComponent } from 'b14-table'
+import { ExampleComponent, Table } from 'b14-table'
 import 'b14-table/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  const columns = [
+    {
+      Header: 'First Name',
+      accessor: 'firstName'
+    },
+    {
+      Header: 'Last Name',
+      accessor: 'lastName'
+    }
+  ]
+  const data = [
+    {
+      id: 1,
+      firstName: 'Amanda',
+      lastName: 'Linasta',
+      dateOfBirth: '02/04/1980',
+      street: '43 8th Adison',
+      city: 'Addison',
+      state: 'Alabama',
+      stateAbbrev: 'AL',
+      zipCode: '35083',
+      startDate: '02/05/2023',
+      department: 'Sales'
+    }
+  ]
+  return (
+    <div>
+      <ExampleComponent text='Create React Library Example 😄' />
+      <Table columns={columns} data={data} />
+    </div>
+  )
 }
 
 export default App
